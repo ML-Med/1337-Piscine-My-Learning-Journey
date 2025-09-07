dfa4b6a3524e27d5f0466ac80dcc1fd1911298bd
Med <mohamed.laoui@hotmail.com>
2025-09-07 15:22:59 +0100

    added

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void  ft_putchar(char c)
+{
+  write (1,&c,1);
+}
+
+void ft_putnbr(int nb)
+{
+  if (nb == -2147483648)
+  {
+    write (1,"-",1);
+    write (1,"214783648",9);
+    return ;
+  }
+  if (nb < 0)
+  {
+        write (1,"-",1);
+        nb = -nb;
+  }
+  if (nb > 9)
+  {
+    ft_putnbr(nb / 10);
+  }
+  ft_putchar((nb % 10) + '0');
+}
+
+int main()
+{
+  ft_putnbr(-345);
+  ft_putchar('\n');
+  return 0;
+}
