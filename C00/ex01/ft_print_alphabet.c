64d09131c5fa12517031811afff8e3ae1c6a9caf
Med <mohamed.laoui@hotmail.com>
2025-09-07 13:53:59 +0100

    added

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+  write (1,&c,1);
+}
+void ft_print_alphabet(void)
+{
+  char x;
+  x = 'a';
+
+  while(x <= 'z'){
+    ft_putchar(x);
+    x++;
+  }
+  write (1,"\n",1);
+}
+
+int main()
+{
+  ft_print_alphabet();
+  return 0;
+}
